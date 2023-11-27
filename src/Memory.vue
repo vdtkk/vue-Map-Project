38e527760949c75ba57130b3f2eff5c62334af02
vedat <vdtkk@hotmail.com>
2023-11-27 17:24:58 +0300

    ilk commit

---
+++ src/Memory.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+
+<p class="text-center"> Bu Memory Component</p>
+
+</div>
+
+</template>
+<script>
+ 
+
+
+</script>
+<style scoped>
+
+div {
+
+border: 1px solid #ccc;
+box-shadow: 1px 1px 2px #666;
+padding: 10px 20px;
+margin-top:20px ;
+
+}
+
+</style>
